6d36580d79423edee0c580497ff367b1ab3521eb
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-03-24 14:16:03 +0100

    dashboard: books as strings

--- book-rating/src/app/dashboard/dashboard.component.ts
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  books: string[];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.books = ['Angular', 'AngularJS', 'React'];
   }
 
 }
