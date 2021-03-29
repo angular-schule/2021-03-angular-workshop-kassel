570307d87fb60d8b0fd09f170781172c4271de19
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-03-29 13:37:40 +0200

    gruppenaufgabe forms

--- book-rating/src/app/books/create-book/create-book.component.ts
+++ book-rating/src/app/books/create-book/create-book.component.ts
@@ -11,11 +11,27 @@ export class CreateBookComponent {
   bookForm = new FormGroup({
     isbn: new FormControl('', [Validators.required, Validators.minLength(3)]),
     title: new FormControl('', Validators.required),
-    description: new FormControl()
+    description: new FormControl(),
+    price: new FormControl(),
   });
 
   isValid(name: string): boolean {
     const control = this.bookForm.get(name);
     return control.touched && control.invalid;
+  }
+
+  submitForm(): void {
+    const newBook = {
+      ...this.bookForm.value,
+      rating: 1
+    };
+
+    // ???
+    // 1. Erstelle ein Event mit dem Namen `create``
+    // 2. Feuer das Event mit dem neuen Buch
+    // 3. Empfange das Buch im Dashboard
+    // 4. Füge das neue Buch dem Buch-Array hinzu (Unveränderbarkeit beachten!)
+
+    this.bookForm.reset();
   }
 }
