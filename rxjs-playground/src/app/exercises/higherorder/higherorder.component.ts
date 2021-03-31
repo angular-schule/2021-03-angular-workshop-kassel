6cd3d14977ed88a1234f5604164f09915cd44848
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-03-31 11:51:45 +0200

    rxjs: handon higherorder

--- rxjs-playground/src/app/exercises/higherorder/higherorder.component.ts
+++ rxjs-playground/src/app/exercises/higherorder/higherorder.component.ts
@@ -30,6 +30,7 @@ export class HigherorderComponent implements OnInit {
     /**************!!**************/
 
     this.result$ = this.source$.pipe(
+      mergeMap(tier => this.es.echo(tier))
     );
 
     /**************!!**************/
